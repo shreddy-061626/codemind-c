131c9ef9df7e1c28b3139f9788598b6753591eea
shreddy-061626 <119720702+shreddy-061626@users.noreply.github.com>
2022-12-03 21:31:41 +0530

    //neon number #include<stdio.h> int main() {     int n,s=0,q,r;     scanf("%d",&n);     q=(n*n);     while(q!=0)     {         r=q%10;         s=s+r;         q=q/10;     }     if(s==n)     {         printf("Neon Number");
        }
        else
        {
            printf("Not Neon Number");
        }
    }

---
+++ Neon_Number.c
@@ -0,0 +1,23 @@
+//neon number
+#include<stdio.h>
+int main()
+{
+    int n,s=0,q,r;
+    scanf("%d",&n);
+    q=(n*n);
+    while(q!=0)
+    {
+        r=q%10;
+        s=s+r;
+        q=q/10;
+    }
+    if(s==n)
+    {
+        printf("Neon Number");
+        
+    }
+    else
+    {
+        printf("Not Neon Number");
+    }
+}

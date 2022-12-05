136b2b03c6605acbdf3e8ff90ad1c09971cd3f29
shreddy-061626 <119720702+shreddy-061626@users.noreply.github.com>
2022-12-05 21:43:20 +0530

    #include<stdio.h> int main() {     int n,y,w;     scanf("%d",&n);     y=n/365;     w=n%365/7;     printf("%d %d",y,w); }

--- Days_into_years,_weeks:.c
+++ Days_into_years,_weeks:.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 int main()
 {
-    int n,w,y;
- scanf("%d",&n);
-  w=(n%365)/7;
-  y=n/365;
-  printf("%d
+    int n,y,w;
+    scanf("%d",&n);
+    y=n/365;
+    w=n%365/7;
+    printf("%d
 %d",y,w);
 }
